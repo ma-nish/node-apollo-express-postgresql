eaf70b5f30584ac3e3b5a7b92845bbd7d45cc108
ma-nish <manish.maurya@mail.vinove.com>
2020-01-28 17:20:50 +0530

    heroku deploy

--- src/Models/index.js
+++ src/Models/index.js
@@ -1,14 +1,21 @@
 import Sequelize from 'sequelize';
 
-const sequelize = new Sequelize(
-  process.env.TEST_DATABASE || process.env.DATABASE,
-  process.env.DATABASE_USER,
-  process.env.DATABASE_PASSWORD,
-  {
-    host: 'localhost',
+let sequelize;
+
+if (process.env.DATABASE_URL) {
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'postgres',
-  },
-);
+  });
+} else {
+  sequelize = new Sequelize(
+    process.env.TEST_DATABASE || process.env.DATABASE,
+    process.env.DATABASE_USER,
+    process.env.DATABASE_PASSWORD,
+    {
+      dialect: 'postgres',
+    },
+  );
+}
 
 // const sequelize = new Sequelize("postgres://postgres:postgres@localhost/mydb", {
 //   dialect: 'postgres'
--- src/index.js
+++ src/index.js
@@ -30,6 +30,8 @@ const getMe = async req => {
 };
 
 const server = new ApolloServer({
+  introspection: true,
+  playground: true,
   typeDefs: schema,
   resolvers,
   formatError: error => {
@@ -79,14 +81,16 @@ const httpServer = http.createServer(app);
 server.installSubscriptionHandlers(httpServer);
 
 const isTest = !!process.env.TEST_DATABASE;
+const isProduction = !!process.env.DATABASE_URL;
+const port = process.env.PORT || 8000;
 
-sequelize.sync({ force: isTest }).then(async () => {
-  if (isTest) {
+sequelize.sync({ force: isTest || isProduction }).then(async () => {
+  if (isTest || isProductionI) {
     createUsersWithMessages(new Date());
   }
 
-  httpServer.listen({ port: 8000 }, () => {
-    console.log(`🚀  Server ready at http://localhost:8000/graphql`);
+  httpServer.listen({ port }, () => {
+    console.log(`Apollo Server on http://localhost:${port}/graphql`);
   });
 });
 
